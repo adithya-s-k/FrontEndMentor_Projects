a4ea28b96c7a0e40fba8f16b6eae0a9fe20e0ce5
Adithya S K <adithyaskolavi@gmail.com>
2022-01-27 23:46:15 +0530

    Huddle landing page challenge html done - Adithya S K

--- Calculator/script.js
+++ Calculator/script.js
@@ -9,7 +9,7 @@ function clearScreen() {
    }
    // This function evaluates the expression and return result
    function calculate() {
-    var p = document.getElementById("result").value;
+    var p = document.getElementById("button").value;
     var q = eval(p);
     document.getElementById("result").value = q;
    }
